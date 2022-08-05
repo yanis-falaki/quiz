9caef5e18434466187a96b2abb5121f5658c8124
Yanis Falaki <yanisfalaki@outlook.com>
2022-08-05 01:14:22 -0400

    Added question list & logic

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
+import { useState } from 'react';
 import './App.css';
+import qList from './Questions.jsx'
 
 function App() {
+  const [currentQuestion, setCurrentQuestion] = useState(0)
+  const [isRevealed, setRevealed] = useState(false)
+
+  const checkAnswer = isCorrect => {
+    setRevealed(true)
+  }
+
   return (
     <div className="App">
       <div id='wrapper'>
-        <div id='question'>
+        <div id='box'>
           <div id='questions'>
-            <h1>Question 1/4</h1>
-            <p>What is the capital of France?</p>
+            <h1>Question {currentQuestion + 1}/{qList.length}</h1>
+            <p>{qList[currentQuestion].question}</p>
             <button id='next'>Next Question</button>
           </div>
           <div id='answers'>
-            <button className='answer'>Paris</button>
-            <button className='answer'>London</button>
-            <button className='answer'>Africa</button>
-            <button className='answer'>Peru</button>
+            {qList[currentQuestion].answers.map(dict => 
+              <button 
+                className={'answer ' + (isRevealed ? (dict.correct ? 'correct' : 'incorrect') : "")} 
+                key={dict.text} 
+                onClick={() => checkAnswer(dict.correct)}>
+                  {dict.text}
+              </button>)}
           </div>
         </div>
       </div>
---
+++ src/Questions.jsx
@@ -0,0 +1,31 @@
+const qList = [
+    {
+        question: "What is the capital of France?",
+        answers: [
+            {text: "Paris", correct: true}, 
+            {text: "London", correct: false}, 
+            {text: "Africa", correct: false}, 
+            {text: "Peru", correct: false}
+        ]
+    },
+    {
+        question: "What is the world's most widely used search engine",
+        answers: [
+            {text: "Bing", correct: false}, 
+            {text: "Netscape", correct: false}, 
+            {text: "Google", correct: true}, 
+            {text: "Tor", correct: false}
+        ]
+    },
+    {
+        question: "What highschool did Yanis Falaki attend?",
+        answers: [
+            {text: "Royal West", correct: false}, 
+            {text: "Villa Maria", correct: true}, 
+            {text: "Loyola", correct: false}, 
+            {text: "LaurenHill", correct: true}
+        ]
+    }
+]
+
+export default qList
