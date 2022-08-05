ec74913052c59d4c93a061aa6bee872ba4324b74
Yanis Falaki <yanisfalaki@outlook.com>
2022-08-05 00:18:23 -0400

    Completed UI

--- src/App.js
+++ src/App.js
@@ -3,7 +3,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div id='wrapper'>
+        <div id='question'>
+          <div id='questions'>
+            <h1>Question 1/4</h1>
+            <p>What is the capital of France?</p>
+            <button id='next'>Next Question</button>
+          </div>
+          <div id='answers'>
+            <button className='answer'>Paris</button>
+            <button className='answer'>London</button>
+            <button className='answer'>Africa</button>
+            <button className='answer'>Peru</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
